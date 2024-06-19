92305c4d0657f33465ec674d0c667b68f19ba894
John the Fox <joao.chivite@gmail.com>
2024-06-18 22:06:43 -0300

    book and user seems to be working alright

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,14 @@ import di.ServiceLocator;
 
 import javax.swing.*;
 
+
+/*
+
+JOÃO PEDRO PASCHOALINI - RA 837759
+JOÃO VITOR CHIVITE - RA 768343
+WILLIAM RAFAEL PINTO - RA 838747
+
+ */
 public class Main {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
